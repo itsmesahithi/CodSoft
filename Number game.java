a91ecbc09c479eca6fbe56c131ac0232edb7fd3a
Miriyala Neeraja Sri Lakshmi Sahithi <144018577+itsmesahithi@users.noreply.github.com>
2024-06-23 13:05:41 +0530

    Update Number game.java

--- Number game.java
+++ Number game.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 public class NumberGame {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int numberToGuess = 42; // The number to guess
-    int numberOfTries = 0; // Number of tries the user has made
-
-    while (true) {
+    Scanner scanner=new Scanner(System.in);
+    int numberToGuess=36; 
+    int numberOfTries=0; 
+     while (true) {
       System.out.println("Guess a number between 1 and 100:");
-      int guess = scanner.nextInt();
+      int guess=scanner.nextInt();
       numberOfTries++;
-
-      if (guess == numberToGuess) {
-        System.out.println("Congratulations! You found the number in " + numberOfTries + " tries.");
+       if (guess==numberToGuess) {
+        System.out.println("Congratulations..."); 
+        System.out.println("You found the number in " + numberOfTries + " tries");
         break;
-      } else if (guess < numberToGuess) {
+      } else if (guess<numberToGuess) {
         System.out.println("Too low! Try again.");
       } else {
         System.out.println("Too high! Try again.");
-      }
-    }
-  }
+      }
+    }
+  }
 }
